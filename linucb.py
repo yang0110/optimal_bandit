942b6f681df2a9af989bb95908a5f43e67fd0245
Kaige Yang <kaige.yang0110@gmail.com>
2020-01-13 20:51:53 +0000

    Add files via upload

--- linucb.py
+++ linucb.py
@@ -6,8 +6,7 @@ import scipy
 import os 
 
 class LINUCB():
-	def __init__(self, dimension, iteration, item_num, item_feature_matrix, true_user_feature, true_payoffs, gaps, best_arm, best_payoff, alpha, delta, sigma, state):
-		self.state=state
+	def __init__(self, dimension, iteration, item_num, item_feature_matrix, true_user_feature, true_payoffs, gaps, best_arm, best_payoff, alpha, delta, sigma):
 		self.dimension=dimension
 		self.iteration=iteration
 		self.item_num=item_num
@@ -37,8 +36,9 @@ class LINUCB():
 		self.est_gaps_ucb=np.zeros((self.iteration, self.item_num))
 		self.best_index=np.zeros((self.iteration, self.item_num))
 		self.ucb_matrix=np.zeros((self.iteration, self.item_num))
+		self.true_ucb_matrix=np.zeros((self.iteration, self.item_num))
 		self.payoff_error_matrix=np.zeros((self.iteration, self.item_num))
-		self.item_counter=np.ones(self.item_num)
+		self.item_counter=np.zeros(self.item_num)
 		self.ucb_list=np.zeros(self.iteration)
 		self.true_ucb_list=np.zeros(self.iteration)
 
@@ -51,12 +51,11 @@ class LINUCB():
 		self.real_beta_list.extend([self.real_beta])
 
 	def select_item(self, time):
-		item_fs=self.item_feature_matrix
+		item_fs=self.item_feature_matrix.copy()
 		estimated_payoffs=np.zeros(self.item_num)
 		mean_y=np.zeros(self.item_num)
 		cov_inv=np.linalg.pinv(self.user_cov)
 		self.update_beta()
-		self.beta=self.beta*self.state
 		for j in range(self.item_num):
 			x=item_fs[j]
 			x_norm=np.sqrt(np.dot(np.dot(x, cov_inv),x))
@@ -65,7 +64,8 @@ class LINUCB():
 			mean_y[j]=np.dot(x, self.user_feature)
 			estimated_payoffs[j]=est_y
 			self.ucb_matrix[time, j]=self.beta*x_norm
-			self.index_matrix[time, j]=est_y
+			self.true_ucb_matrix[time, j]=self.real_beta*x_norm
+			self.index_matrix[time, j]=np.dot(x, self.user_feature)+self.beta*x_norm
 			self.x_norm_matrix[time, j]=x_norm
 			self.mean_matrix[time, j]=np.dot(x, self.user_feature)
 			self.payoff_error_matrix[time, j]=np.abs(self.mean_matrix[time, j]-self.true_payoffs[j])
@@ -97,11 +97,10 @@ class LINUCB():
 		learning_error_list=[]
 		for time in range(iteration):	
 			print('time/iteration', time, iteration,'~~~LinUCB')
-			#self.delta=1/(1+time)
 			true_payoff, selected_item_feature, regret=self.select_item(time)
 			self.update_user_feature(true_payoff, selected_item_feature)
 			error=np.linalg.norm(self.user_feature-self.true_user_feature)
 			cumulative_regret.extend([cumulative_regret[-1]+regret])
 			learning_error_list.extend([error])
 
-		return cumulative_regret[1:], learning_error_list, self.beta_list, self.real_beta_list, self.item_index_selected, self.index_matrix, self.x_norm_matrix, self.mean_matrix, self.gaps_ucb, self.est_gaps_ucb, self.best_index, self.ucb_matrix, self.payoff_error_matrix, self.ucb_list, self.true_ucb_list
+		return cumulative_regret[1:], learning_error_list, self.beta_list, self.real_beta_list, self.item_index_selected, self.index_matrix, self.x_norm_matrix, self.mean_matrix, self.gaps_ucb, self.est_gaps_ucb, self.best_index, self.ucb_matrix, self.true_ucb_matrix, self.payoff_error_matrix, self.ucb_list, self.true_ucb_list
